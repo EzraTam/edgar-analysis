5ef6c175038285114a9c114d27a934cf3607ec73
Ezra Tampubolon <ezra.tampubolon@googlemail.com>
2022-11-14 21:42:37 +0100

    Add config for loan receivables

--- config.py
+++ config.py
@@ -7,4 +7,10 @@ df_config={
         "ratio":[{"res_col_nm":"interest_rate_deposits","numer_col_nm":"interest_expense_deposits",
                         "denom_col_nm":"interest_bearing_deposit_liabilities"}]
     }
+    ,
+    "loan_receivables":{
+        "yearly_change": ["loan_receivables","interest_loans"],
+        "ratio":[{"res_col_nm":"interest_rate_loan_receivables","numer_col_nm":"interest_loans",
+        "denom_col_nm":"loan_receivables"}]
+    }
 }
