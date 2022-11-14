2c8fc218396148ccb2c9407bc81015f240963612
Ezra Tampubolon <ezra.tampubolon@googlemail.com>
2022-11-14 21:43:01 +0100

    add possibility to add tags

--- edgar_fin_data_services.py
+++ edgar_fin_data_services.py
@@ -10,7 +10,7 @@ from typing import List, Optional, Dict, Union
 
 from functools import reduce
 
-from icecream import ic
+# from icecream import ic
 
 import seaborn as sns
 
@@ -181,7 +181,7 @@ class FinancialAnalyze:
 
         # Raise error if the init_df option is chosen, dataframe already
         # exists but overwrite_df option is not chosen
-        if init_df and df_nm in self.dict_df.keys() and not overwrite_df:
+        if init_df and df_nm in self.dict_df and not overwrite_df:
             raise ValueError(
                 f"""
                 DF {df_nm} already exists.
@@ -191,7 +191,7 @@ class FinancialAnalyze:
                 """
             )
 
-        if df_nm in self.dict_df.keys():
+        if df_nm in self.dict_df:
             # Raise error if the init_col option is chosen, column already
             # exists but overwrite_col option is not chosen
             if init_col and col_nm in self.dict_df[df_nm].columns and not overwrite_col:
@@ -319,7 +319,7 @@ class FinancialAnalyze:
         for to_compute in list_to_compute:
             self.dict_df[df_nm][to_compute["res_col_nm"]] = self._compute_ratio(
                 self.dict_df[df_nm][to_compute["numer_col_nm"]],
-                self.dict_df[df_nm][to_compute["denom_col_nm"]],
+                self.dict_df[df_nm][to_compute["denom_col_nm"]]
             )
 
     def generate_df(
@@ -338,22 +338,39 @@ class FinancialAnalyze:
                     'drop': col_nm , list rows
         """
         for gen in how_gen:
-            init_df = True if how_gen.index(gen) == 0 else False
-            init_col = True if how_gen.index(gen) > 0 else False
 
-            self.add_yearly_data_norm_to_col(
-                tags=gen["list_tags"],
-                col_nm=gen["col_nm"],
-                df_nm=df_nm,
-                init_df=init_df,
-                init_col=init_col,
-            )
+            if "list_tags" in gen: 
+                # Initialize DF in the beginning of the workflow
+                init_df = True if how_gen.index(gen) == 0 else False
 
+                # Initialize DF columns
+                init_col = True if how_gen.index(gen) > 0 else False
+
+                self.add_yearly_data_norm_to_col(
+                    tags=gen["list_tags"],
+                    col_nm=gen["col_nm"],
+                    df_nm=df_nm,
+                    init_df=init_df,
+                    init_col=init_col,
+                )
+
+            if "add_tags" in gen:
+                _li_df=[self.extract_yearly_data_norm(tag=tag, col_nm=tag) for tag in gen["add_tags"]]
+                _df_temp=self._merge_yearly_data(li_df_merge=_li_df)
+                _df_temp[gen["col_nm"]]=_df_temp.sum(axis=1)
+                _df_temp=_df_temp.drop(columns=gen["add_tags"])
+                self.dict_df[df_nm]=self._merge_yearly_data(li_df_merge=[self.dict_df[df_nm],_df_temp])
+
+            
             if "drop" in gen:
                 self.dict_df[df_nm] = (
                     self.dict_df[df_nm].drop(gen["drop"]).reset_index(drop=True)
                 )
+
+
         self.dict_df[df_nm] = self.dict_df[df_nm].dropna()
+
+        # Additional generation of columns
 
         if df_nm in df_config:
             add_gen = df_config[df_nm]
@@ -371,33 +388,60 @@ class FinancialAnalyze:
             self.compute_ratio_multiple(list_to_compute=add_gen["ratio"], df_nm=df_nm)
 
     @staticmethod
-    def _pick_year_data(df:pd.DataFrame,col_nm:str,year:int):
-        return df[df["year"]==year].reset_index(drop=True)[col_nm][0]
+    def _pick_year_data(df: pd.DataFrame, col_nm: str, year: int):
+        return df[df["year"] == year].reset_index(drop=True)[col_nm][0]
 
     # CAGR
     @staticmethod
-    def _compute_cagr(begin_value:float,end_value:float,num_years:int)->float:
-        return (pow(end_value/begin_value,1/num_years)-1)*100
-    
-    def compute_yearly_cagr(self,df_nm:str,col_nm:str, interval:Dict[str,int],round_num:Optional[int]=None):
-        _df_temp=self.dict_df[df_nm]
-        begin_value=self._pick_year_data(_df_temp,col_nm=col_nm,year=interval["begin"])
-        end_value=self._pick_year_data(_df_temp,col_nm=col_nm,year=interval["end"])
-        num_years=interval["end"]-interval["begin"]
-        result= self._compute_cagr(begin_value=begin_value,end_value=end_value,num_years=num_years)
+    def _compute_cagr(begin_value: float, end_value: float, num_years: int) -> float:
+        return (pow(end_value / begin_value, 1 / num_years) - 1) * 100
 
-        return result if not round_num else round(result,2) 
+    def compute_yearly_cagr(
+        self,
+        df_nm: str,
+        col_nm: str,
+        interval: Dict[str, int],
+        round_num: Optional[int] = None,
+    ):
+        _df_temp = self.dict_df[df_nm]
+        begin_value = self._pick_year_data(
+            _df_temp, col_nm=col_nm, year=interval["begin"]
+        )
+        end_value = self._pick_year_data(_df_temp, col_nm=col_nm, year=interval["end"])
+        num_years = interval["end"] - interval["begin"]
+        result = self._compute_cagr(
+            begin_value=begin_value, end_value=end_value, num_years=num_years
+        )
 
-    def compute_stats(self,df_nm:str,col_nm:str, interval:Dict[str,int],round_num:Optional[int]=None,show_pd:Optional[bool]=False):
-        stat_to_extract=["mean","median","max","min"]
+        return result if not round_num else round(result, 2)
 
-        _df_temp=self.dict_df[df_nm]
-        _ser_temp=_df_temp[_df_temp["year"].isin(range(interval["begin"],interval["end"]+1))][col_nm]
+    def compute_stats(
+        self,
+        df_nm: str,
+        col_nm: str,
+        interval: Dict[str, int],
+        round_num: Optional[int] = None,
+        show_pd: Optional[bool] = False,
+    ):
+        stat_to_extract = ["mean", "median", "max", "min"]
 
-        stat={stat_method: getattr(pd.Series, stat_method)(_ser_temp) if not round_num else round(getattr(pd.Series, stat_method)(_ser_temp),round_num)  for stat_method in stat_to_extract}
+        _df_temp = self.dict_df[df_nm]
+        _ser_temp = _df_temp[
+            _df_temp["year"].isin(range(interval["begin"], interval["end"] + 1))
+        ][col_nm]
 
-        return stat if not show_pd else pd.DataFrame(data=stat,index=[0])
+        stat = {
+            stat_method: getattr(pd.Series, stat_method)(_ser_temp)
+            if not round_num
+            else round(getattr(pd.Series, stat_method)(_ser_temp), round_num)
+            for stat_method in stat_to_extract
+        }
 
+        return stat if not show_pd else pd.DataFrame(data=stat, index=[0])
+
+    # Other services
+    def delete_df(self,df_nm:str)->None:
+        del self.dict_df[df_nm]
 
     # Services for labels and descriptions
 
@@ -407,6 +451,10 @@ class FinancialAnalyze:
         Usage e.g. output_entry('FeesAndCommissionsCreditCards','description')
         """
         return self.data["facts"]["us-gaap"][position][key]
+
+    def show_description(self,tag:str)->str:
+        return self.output_entry(position=tag, key= "description")
+
 
     def find_in_dict_entry(self, key: str, query: str) -> list:
         """
@@ -455,6 +503,8 @@ class InterCompaniesAnalyze:
         different companies
         """
 
+        li_comp_obj_compare=[comp_obj for comp_obj in self.li_comp_obj if df_name in comp_obj.dict_df]
+
         def _extract_quantity(comp_obj: FinancialAnalyze):
             return comp_obj.dict_df[df_name][["year", quantity]].rename(
                 columns={quantity: comp_obj.comp_nm}
@@ -462,30 +512,61 @@ class InterCompaniesAnalyze:
 
         self.dict_df[quantity] = reduce(
             lambda left, right: pd.merge(left, right, on=["year"], how="outer"),
-            map(_extract_quantity, self.li_comp_obj),
+            map(_extract_quantity, li_comp_obj_compare),
         )
 
-    def compute_cagr(self,df_nm:str,col_nm:str,interval:Dict[str,int],round_num:Optional[int]=None):
+    def compute_cagr(
+        self,
+        df_nm: str,
+        col_nm: str,
+        interval: Dict[str, int],
+        round_num: Optional[int] = None,
+    ):
 
-        result_cagr=[
+        result_cagr = [
             (
                 comp_obj.comp_nm,
-                comp_obj.compute_yearly_cagr(df_nm=df_nm,col_nm=col_nm, interval=interval,round_num=round_num)
-            ) 
+                comp_obj.compute_yearly_cagr(
+                    df_nm=df_nm, col_nm=col_nm, interval=interval, round_num=round_num
+                ),
+            )
             for comp_obj in self.li_comp_obj
         ]
 
-        return pd.DataFrame(result_cagr, columns=["company","cagr"])
+        return pd.DataFrame(result_cagr, columns=["company", "cagr"])
 
-    def compute_stats(self,df_nm:str,col_nm:str,interval:Dict[str,int],round_num:Optional[int]=None):
-        dfs=[comp_obj.compute_stats(df_nm=df_nm,col_nm=col_nm,interval=interval,round_num=round_num,show_pd=True) for comp_obj in self.li_comp_obj]
-        columns=dfs[0].columns
-        df=pd.concat(dfs)
-        df["company"]=[comp_obj.comp_nm for comp_obj in self.li_comp_obj]
-        return df.reset_index(drop=True)[["company",*columns]]
+    def compute_stats(
+        self,
+        df_nm: str,
+        col_nm: str,
+        interval: Dict[str, int],
+        round_num: Optional[int] = None,
+    ) -> pd.DataFrame:
+        """Compute statistics of certain column
 
+        Args:
+            df_nm (str): Name of the DF
+            col_nm (str): Column for which the stats should be computed
+            interval (Dict[str,int]): Year interval
+            round_num (Optional[int], optional): Round to hom many digit?. Defaults to None.
 
-
+        Returns:
+            pd.DataFrame: _description_
+        """
+        dfs = [
+            comp_obj.compute_stats(
+                df_nm=df_nm,
+                col_nm=col_nm,
+                interval=interval,
+                round_num=round_num,
+                show_pd=True,
+            )
+            for comp_obj in self.li_comp_obj
+        ]
+        columns = dfs[0].columns
+        df = pd.concat(dfs)
+        df["company"] = [comp_obj.comp_nm for comp_obj in self.li_comp_obj]
+        return df.reset_index(drop=True)[["company", *columns]]
 
     def plot_df(
         self, key: str, xlabel: str, ylabel: str, width: float, height: float
